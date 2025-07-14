6997df9836cd5ac16908e9312153729fc8c3d768
atumferoz <junececilia03@gmail,com>
2025-07-14 13:02:41 +0100

    debugging

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const LocalStrategy = require('passport').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
